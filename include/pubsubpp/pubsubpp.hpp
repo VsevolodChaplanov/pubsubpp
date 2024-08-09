f5dc763ed78edd4c4977efcfa79745b2953ea4d3
VChaplanov <woornad@gmail.com>
2024-08-09 12:51:52 +0300

    patch - implicit const qualifier for constexpr virtual function, fix clang(cannot bind to a value of unrelated type) - lint fixes - add some new py packages for development

--- include/pubsubpp/pubsubpp.hpp
+++ include/pubsubpp/pubsubpp.hpp
@@ -30,11 +30,11 @@ namespace ps {
 	 * @details specialization must contain std::tuple<Args...>
 	 * @tparam TEvent
 	 */
-	template<typename TEvent> struct EventTraits {};
+	template<typename TEvent> struct event_traits {};
 
 	template<typename TEvent>
 		requires detail::CEvent<TEvent>
-	struct EventTraits<TEvent> {
+	struct event_traits<TEvent> {
 		using args_t = typename TEvent::args_t;
 	};
 
@@ -42,34 +42,34 @@ namespace ps {
 	 * @tparam Id -> differ two types with the same arguments set
 	 * @tparam TEventArgs -> event arguments
 	 */
-	template<auto Id, typename... TEventArgs> struct CreateEvent {
+	template<auto Id, typename... TEventArgs> struct create_event {
 		using args_t = std::tuple<TEventArgs...>;
 	};
 
 	template<typename TEvent>
 	concept CTrait = requires {
-		typename EventTraits<TEvent>;		  // template defined
-		typename EventTraits<TEvent>::args_t; // template has arguments
-	} && is_specialization_v<typename EventTraits<TEvent>::args_t, std::tuple>;
+		typename event_traits<TEvent>;		  // template defined
+		typename event_traits<TEvent>::args_t; // template has arguments
+	} && is_specialization_v<typename event_traits<TEvent>::args_t, std::tuple>;
 
-	template<CTrait TEvent> struct SingleEventManager;
+	template<CTrait TEvent> struct single_event_manager;
 
-	template<CTrait... TEvents> struct EventsManager;
+	template<CTrait... TEvents> struct events_manager;
 
-	template<CTrait TEvents> struct ISingleEventSubscriber;
+	template<CTrait TEvents> struct i_single_event_subscriber;
 
 	template<typename T> struct Dummy;
 
-	template<CTrait TEvent> struct SingleEventManager {
-		using single_event_consumer_t = ISingleEventSubscriber<TEvent>;
-		using args_t = typename EventTraits<TEvent>::args_t;
+	template<CTrait TEvent> struct single_event_manager {
+		using single_event_consumer_t = i_single_event_subscriber<TEvent>;
+		using args_t = typename event_traits<TEvent>::args_t;
 
-		constexpr SingleEventManager() noexcept = default;
+		constexpr single_event_manager() noexcept = default;
 
-		constexpr SingleEventManager(const SingleEventManager&) = default;
-		constexpr SingleEventManager(SingleEventManager&&) = default;
-		constexpr SingleEventManager& operator=(const SingleEventManager&) = default;
-		constexpr SingleEventManager& operator=(SingleEventManager&&) = default;
+		constexpr single_event_manager(const single_event_manager&) = default;
+		constexpr single_event_manager(single_event_manager&&) = default;
+		constexpr single_event_manager& operator=(const single_event_manager&) = default;
+		constexpr single_event_manager& operator=(single_event_manager&&) = default;
 
 		constexpr void add_subscriber(single_event_consumer_t* subscriber) & { m_subscribers.push_back(subscriber); }
 
@@ -80,35 +80,35 @@ namespace ps {
 			}
 		}
 
-		constexpr ~SingleEventManager() noexcept = default;
+		constexpr ~single_event_manager() noexcept = default;
 
 	private:
 		std::vector<single_event_consumer_t*> m_subscribers;
 	};
 
-	template<CTrait... TEvents> struct EventsManager : public SingleEventManager<TEvents>... {
-		constexpr EventsManager() noexcept = default;
+	template<CTrait... TEvents> struct events_manager : public single_event_manager<TEvents>... {
+		constexpr events_manager() noexcept = default;
 
-		constexpr EventsManager(const EventsManager&) = default;
-		constexpr EventsManager(EventsManager&&) = default;
-		constexpr EventsManager& operator=(const EventsManager&) = default;
-		constexpr EventsManager& operator=(EventsManager&&) = default;
+		constexpr events_manager(const events_manager&) = default;
+		constexpr events_manager(events_manager&&) = default;
+		constexpr events_manager& operator=(const events_manager&) = default;
+		constexpr events_manager& operator=(events_manager&&) = default;
 
-		constexpr ~EventsManager() = default;
+		constexpr ~events_manager() = default;
 	};
 
-	template<typename TSelf, CTrait TEvent> struct SingleEventPublisher {
-		using event_manager_t = SingleEventManager<TEvent>;
+	template<typename TSelf, CTrait TEvent> struct single_event_publisher {
+		using event_manager_t = single_event_manager<TEvent>;
 
-		constexpr explicit SingleEventPublisher(event_manager_t& manager) noexcept
+		constexpr explicit single_event_publisher(event_manager_t& manager) noexcept
 			: m_manager(std::addressof(manager)) {}
 
-		constexpr SingleEventPublisher(const SingleEventPublisher&) = default;
-		constexpr SingleEventPublisher(SingleEventPublisher&&) noexcept = default;
-		constexpr SingleEventPublisher& operator=(const SingleEventPublisher&) = default;
-		constexpr SingleEventPublisher& operator=(SingleEventPublisher&&) noexcept = default;
+		constexpr single_event_publisher(const single_event_publisher&) = default;
+		constexpr single_event_publisher(single_event_publisher&&) noexcept = default;
+		constexpr single_event_publisher& operator=(const single_event_publisher&) = default;
+		constexpr single_event_publisher& operator=(single_event_publisher&&) noexcept = default;
 
-		using traits_t = EventTraits<TEvent>;
+		using traits_t = event_traits<TEvent>;
 		using notify_args_t = typename traits_t::args_t;
 
 		template<typename... Args> constexpr void single_dispatch(Args&&... args) & {
@@ -117,35 +117,35 @@ namespace ps {
 		}
 
 	protected:
-		constexpr ~SingleEventPublisher() noexcept = default;
+		constexpr ~single_event_publisher() noexcept = default;
 
 	private:
 		event_manager_t* m_manager{nullptr};
 	};
 
-	template<typename TSelf, CTrait... Events> struct EventsPublisher : public SingleEventPublisher<TSelf, Events>... {
-		using events_manager_t = EventsManager<Events...>;
+	template<typename TSelf, CTrait... Events> struct events_publisher : public single_event_publisher<TSelf, Events>... {
+		using events_manager_t = events_manager<Events...>;
 
 		template<typename TEventManager>
-			requires(std::is_base_of_v<SingleEventManager<Events>, TEventManager> && ...)
-		constexpr explicit EventsPublisher(TEventManager& manager) noexcept
-			: SingleEventPublisher<TSelf, Events>{manager}... {}
+			requires(std::is_base_of_v<single_event_manager<Events>, TEventManager> && ...)
+		constexpr explicit events_publisher(TEventManager& manager) noexcept
+			: single_event_publisher<TSelf, Events>{manager}... {}
 
-		constexpr explicit EventsPublisher(events_manager_t& manager) noexcept
-			: SingleEventPublisher<TSelf, Events>{manager}... {}
+		constexpr explicit events_publisher(events_manager_t& manager) noexcept
+			: single_event_publisher<TSelf, Events>{manager}... {}
 
-		constexpr EventsPublisher(const EventsPublisher&) = default;
-		constexpr EventsPublisher(EventsPublisher&&) = default;
-		constexpr EventsPublisher& operator=(const EventsPublisher&) = default;
-		constexpr EventsPublisher& operator=(EventsPublisher&&) = default;
+		constexpr events_publisher(const events_publisher&) = default;
+		constexpr events_publisher(events_publisher&&) = default;
+		constexpr events_publisher& operator=(const events_publisher&) = default;
+		constexpr events_publisher& operator=(events_publisher&&) = default;
 
 		template<CTrait TEvent, typename... Args>
 			requires(std::is_same_v<TEvent, Events> || ...)
 		constexpr void dispatch(Args&&... args) & {
-			SingleEventPublisher<TSelf, TEvent>::single_dispatch(std::forward<Args>(args)...);
+			single_event_publisher<TSelf, TEvent>::single_dispatch(std::forward<Args>(args)...);
 		}
 
-		constexpr ~EventsPublisher() = default;
+		constexpr ~events_publisher() = default;
 	};
 
 	/**
@@ -157,44 +157,46 @@ namespace ps {
 	 */
 	template<typename TSelf, typename TEvent, typename... Args>
 	concept CConsumer =
-		CTrait<TEvent> && std::is_same_v<typename EventTraits<TEvent>::args_t, std::tuple<Args...>> &&
+		CTrait<TEvent> && std::is_same_v<typename event_traits<TEvent>::args_t, std::tuple<Args...>> &&
 		requires(TSelf self, Args&&... args) { self.template consume_event<TEvent>(std::forward<Args>(args)...); };
 
-	template<CTrait TEvent> struct ISingleEventSubscriber {
-		using single_event_manager_t = SingleEventManager<TEvent>;
-		using traits_t = EventTraits<TEvent>;
+	template<CTrait TEvent> struct i_single_event_subscriber {
+		using single_event_manager_t = single_event_manager<TEvent>;
+		using traits_t = event_traits<TEvent>;
 		using notify_args_t = typename traits_t::args_t;
 
-		constexpr ISingleEventSubscriber(single_event_manager_t& manager) noexcept { manager.add_subscriber(this); }
+		constexpr explicit i_single_event_subscriber(single_event_manager_t& manager) noexcept {
+			manager.add_subscriber(this);
+		}
 
-		constexpr ISingleEventSubscriber(const ISingleEventSubscriber&) = default;
-		constexpr ISingleEventSubscriber(ISingleEventSubscriber&&) = default;
-		constexpr ISingleEventSubscriber& operator=(const ISingleEventSubscriber&) = default;
-		constexpr ISingleEventSubscriber& operator=(ISingleEventSubscriber&&) = default;
+		constexpr i_single_event_subscriber(const i_single_event_subscriber&) = default;
+		constexpr i_single_event_subscriber(i_single_event_subscriber&&) = default;
+		constexpr i_single_event_subscriber& operator=(const i_single_event_subscriber&) = default;
+		constexpr i_single_event_subscriber& operator=(i_single_event_subscriber&&) = default;
 
-		constexpr virtual void single_consume(notify_args_t args) & = 0;
+		virtual void single_consume(notify_args_t args) & = 0;
 
-		constexpr virtual ~ISingleEventSubscriber() noexcept = default;
+		constexpr virtual ~i_single_event_subscriber() noexcept = default;
 	};
 
-	template<typename TSelf, CTrait TEvent> struct SingleEventSubscriber : public ISingleEventSubscriber<TEvent> {
-		using ISingleEventSubscriber<TEvent>::ISingleEventSubscriber;
+	template<typename TSelf, CTrait TEvent> struct single_event_subscriber : public i_single_event_subscriber<TEvent> {
+		using i_single_event_subscriber<TEvent>::i_single_event_subscriber;
 
-		constexpr SingleEventSubscriber(const SingleEventSubscriber&) = default;
-		constexpr SingleEventSubscriber(SingleEventSubscriber&&) = default;
-		constexpr SingleEventSubscriber& operator=(const SingleEventSubscriber&) = default;
-		constexpr SingleEventSubscriber& operator=(SingleEventSubscriber&&) = default;
+		constexpr single_event_subscriber(const single_event_subscriber&) = default;
+		constexpr single_event_subscriber(single_event_subscriber&&) = default;
+		constexpr single_event_subscriber& operator=(const single_event_subscriber&) = default;
+		constexpr single_event_subscriber& operator=(single_event_subscriber&&) = default;
 
-		using typename ISingleEventSubscriber<TEvent>::traits_t;
-		using typename ISingleEventSubscriber<TEvent>::notify_args_t;
+		using typename i_single_event_subscriber<TEvent>::traits_t;
+		using typename i_single_event_subscriber<TEvent>::notify_args_t;
 
-		constexpr void single_consume(notify_args_t args) & override {
+		void single_consume(notify_args_t args) & override {
 			constexpr auto tuple_size = std::tuple_size_v<notify_args_t>;
 			constexpr auto integer_sequence = std::make_index_sequence<tuple_size>{};
 			single_consume_helper(integer_sequence, std::move(args));
 		}
 
-		constexpr ~SingleEventSubscriber() override = default;
+		constexpr ~single_event_subscriber() override = default;
 
 	private:
 		static constexpr auto tuple_size = std::tuple_size_v<notify_args_t>;
@@ -212,28 +214,28 @@ namespace ps {
 	};
 
 	template<typename TSelf, CTrait... TEvents>
-	struct [[nodiscard("dangling pointer on subscription if discarded")]] EventsSubscriber
-		: public SingleEventSubscriber<TSelf, TEvents>... {
+	struct [[nodiscard("dangling pointer on subscription if discarded")]] events_subscriber
+		: public single_event_subscriber<TSelf, TEvents>... {
 		template<typename TEventManager>
-			requires(std::is_base_of_v<SingleEventManager<TEvents>, TEventManager> && ...)
-		explicit constexpr EventsSubscriber(TEventManager& manager) noexcept
-			: SingleEventSubscriber<TSelf, TEvents>{manager}... {}
+			requires(std::is_base_of_v<single_event_manager<TEvents>, TEventManager> && ...)
+		explicit constexpr events_subscriber(TEventManager& manager) noexcept
+			: single_event_subscriber<TSelf, TEvents>{manager}... {}
 
-		explicit constexpr EventsSubscriber(EventsManager<TEvents...>& manager) noexcept
-			: SingleEventSubscriber<TSelf, TEvents>{manager}... {}
+		explicit constexpr events_subscriber(events_manager<TEvents...>& manager) noexcept
+			: single_event_subscriber<TSelf, TEvents>{manager}... {}
 
-		constexpr EventsSubscriber(const EventsSubscriber&) = default;
-		constexpr EventsSubscriber(EventsSubscriber&&) = default;
-		constexpr EventsSubscriber& operator=(const EventsSubscriber&) = default;
-		constexpr EventsSubscriber& operator=(EventsSubscriber&&) = default;
+		constexpr events_subscriber(const events_subscriber&) = default;
+		constexpr events_subscriber(events_subscriber&&) = default;
+		constexpr events_subscriber& operator=(const events_subscriber&) = default;
+		constexpr events_subscriber& operator=(events_subscriber&&) = default;
 
 		template<CTrait TEvent, typename... Args>
 			requires(std::is_same_v<TEvent, TEvents> || ...) && CConsumer<TSelf, TEvent, Args...>
 		constexpr void consume(Args&&... args) & {
-			SingleEventSubscriber<TSelf, TEvent>::single_consume(std::forward_as_tuple(std::forward<Args>(args)...));
+			single_event_subscriber<TSelf, TEvent>::single_consume(std::forward_as_tuple(std::forward<Args>(args)...));
 		}
 
-		constexpr ~EventsSubscriber() override = default;
+		constexpr ~events_subscriber() override = default;
 	};
 } // namespace ps
 
--- test/source/pubsubpp_test.cpp
+++ test/source/pubsubpp_test.cpp
@@ -2,4 +2,128 @@
 
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("Name is pubsubpp", "[library]") {}
+namespace ps {
+	/**
+	 * @brief struct helper to convert enum value to tagged struct
+	 *
+	 * @tparam StateID -> enum value
+	 */
+	template<auto StateID> struct state : public std::integral_constant<std::decay_t<decltype(StateID)>, StateID> {};
+
+	enum struct events {
+		A,
+		B,
+		C,
+	};
+
+	using a_event = state<events::A>;
+	using b_event = state<events::B>;
+
+	struct c_event {
+		using args_t = std::tuple<>;
+	};
+	
+	template<> struct event_traits<a_event> {
+		using args_t = std::tuple<int, std::string>;
+	};
+
+	template<> struct event_traits<b_event> {
+		using args_t = std::tuple<std::string, std::string>;
+	};
+
+	struct ab_events_dispatcher : public events_publisher<ab_events_dispatcher, a_event, b_event> {
+		int a_dispatch_counter{0};
+		int b_dispatch_counter{0};
+
+		using events_publisher<ab_events_dispatcher, a_event, b_event>::events_publisher;
+	};
+
+	struct ab_events_consumer : public events_subscriber<ab_events_consumer, a_event, b_event> { // NOLINT
+		int a_consume_counter{0};
+		int b_consume_counter{0};
+		int c_misses{0};
+
+		using events_subscriber<ab_events_consumer, a_event, b_event>::events_subscriber;
+
+		constexpr ab_events_consumer(const ab_events_consumer&) = default;
+		constexpr ab_events_consumer(ab_events_consumer&&) = default;
+		constexpr ab_events_consumer& operator=(const ab_events_consumer&) = default;
+		constexpr ab_events_consumer& operator=(ab_events_consumer&&) = default;
+
+		constexpr ~ab_events_consumer() override = default;
+
+		template<typename TEvent, typename... Args>
+			requires(std::is_same_v<TEvent, a_event> || std::is_same_v<TEvent, b_event>)
+		void consume_event(Args...) & {
+			c_misses++;
+		}
+	};
+
+	template<> void ab_events_consumer::consume_event<a_event>([[maybe_unused]] int a, std::string b) & {
+		a_consume_counter++;
+		b = "abc";
+	}
+
+	template<>
+	void ab_events_consumer::consume_event<b_event>([[maybe_unused]] std::string a, [[maybe_unused]] std::string b) & {
+		b_consume_counter++;
+	}
+
+	struct a_events_consumer : public events_subscriber<a_events_consumer, a_event> {
+		constexpr a_events_consumer(const a_events_consumer&) = default;
+		constexpr a_events_consumer(a_events_consumer&&) = default;
+		constexpr a_events_consumer& operator=(const a_events_consumer&) = default;
+		constexpr a_events_consumer& operator=(a_events_consumer&&) = default;
+
+		constexpr ~a_events_consumer() override = default;
+
+		int a_consume_counter{0};
+		int c_misses{0};
+
+		using events_subscriber<a_events_consumer, a_event>::events_subscriber;
+
+		template<typename TEvent, typename... Args>
+			requires(std::is_same_v<TEvent, a_event>)
+		constexpr void consume_event(Args...) & {
+			c_misses++;
+		}
+	};
+
+	template<> constexpr void a_events_consumer::consume_event<a_event>(int a, std::string b) & {
+		a_consume_counter++;
+		b = "abc";
+	}
+
+	struct a_events_dispatcher : public events_publisher<a_events_dispatcher, a_event> {
+		int a_dispatch_counter{0};
+
+		using events_publisher<a_events_dispatcher, a_event>::events_publisher;
+	};
+
+	struct c_event_dispatcher : public events_publisher<c_event_dispatcher, c_event> {};
+
+	using ab_events_manager = events_manager<a_event, b_event>;
+
+	TEST_CASE("custom event system") {
+		using namespace std::string_literals;
+		ab_events_manager manager{};
+
+		a_events_consumer a_consumer{manager};
+		ab_events_consumer consumer(manager);
+		ab_events_consumer consumer2(manager);
+
+		ab_events_dispatcher dispatcher{manager};
+		a_events_dispatcher dispatcher2{manager};
+
+		std::string a;
+		int b{5};
+
+		dispatcher2.dispatch<a_event>(b, a);
+
+		dispatcher.dispatch<a_event>(b, a);
+
+		CHECK(consumer.a_consume_counter == 2);
+		CHECK(consumer.b_consume_counter == 0);
+		CHECK(consumer.c_misses == 0);
+	}
+} // namespace ps
